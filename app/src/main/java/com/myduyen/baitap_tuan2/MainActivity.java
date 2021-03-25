35f0f9c222dbabfdbc287fcc06f8b28494180e7e
MyDuyen-jpg <btmduyen121@gmail.com>
2021-03-25 23:25:01 +0700

    bai tap tuan 02

---
+++ app/src/main/java/com/myduyen/baitap_tuan2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myduyen.baitap_tuan2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout2d);
+    }
+}
